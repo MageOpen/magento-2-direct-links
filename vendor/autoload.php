bc10e20d88dd0db41ebce76ef4ee3e308cdacce6
John <sam@mageplaza.com>
2016-07-18 23:14:03 +0700

    Release CE v2.1.0

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitfe4d619b535c8cfd47931d99062bdcfc::getLoader();
+return ComposerAutoloaderInit8a09a4e3946071181749b9d3c872c3db::getLoader();
